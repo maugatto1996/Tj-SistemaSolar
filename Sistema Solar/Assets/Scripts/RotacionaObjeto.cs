85a34170bc9d302dc6bc9ec87ea9bbecf71cce74
Mauricio Gatto <mauricio.ggodoy@senacsp.edu.br>
2019-04-26 21:23:01 -0300

    Criando Rotação e Textura

---
+++ Sistema Solar/Assets/Scripts/RotacionaObjeto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionaObjeto : MonoBehaviour
+{
+
+    public float velocidade = 50f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * velocidade * Time.deltaTime);
+
+    }
+}
